0a07652da03ab86e4d56a193489769510647b37b
killacode <sudarev.n@yandex.ru>
2026-01-22 10:31:27 +0300

    storybook upgrade

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,13 +2,7 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-links',
-    '@storybook/addon-a11y',
-    '@storybook/addon-docs',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-a11y', '@storybook/addon-docs'],
   framework: {
     name: '@storybook/angular',
     options: {},
